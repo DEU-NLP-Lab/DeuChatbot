42e90d0cc975dac8e792d1f5124c1781f565f2cf
Minsu <jminsu084@gmail.com>
2024-04-07 00:15:13 +0900

    refactor : 코드 모듈화

--- deuChatbot/main.py
+++ deuChatbot/main.py
@@ -1,5 +1,6 @@
 import tiktoken  # openAI 에서 제공하는 오픈 소스 토크나이저
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
@@ -17,22 +18,14 @@ def tiktoken_len(text):
     return len(tokens)
 
 
-def db_qna(q):
+def chat_llm(db):
     """
-    벡터저장소에서 질문을 검색해서 적절한 답변을 찾아서 답하도록 하는 함수
-    :param q: 사용자 질문
-    :return: 답변
+    채팅에 사용되는 거대언어모델 생성 함수
+    :param db: 벡터저장소
+    :return: 답변해주는 거대언어모델
     """
-    db = Chroma(persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings())
 
-    docs = db.similarity_search(q)
-
-    # test = db.similarity_search_with_relevance_scores(query, k=5)  # 유사도가 높은 문서 몇개까지 반환받을 건지 설정할 때 k 매개변수 사용 / 유사도 높은 상위 3개 문서를 뽑아서 docs에 반환
-    # for doc in test:
-    #     print("가장 유사한 문서:\n\n {}\n\n".format(doc[0].page_content))
-    #     print("문서 유사도:\n {}".format(doc[1]))
-    #     print("\n-------------------------")
-
+    os.environ['OPENAI_API_KEY'] = "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"  # 테스트 버전일 때
 
     # Retriever 적용
     openai = ChatOpenAI(
@@ -57,61 +50,50 @@ def db_qna(q):
         return_source_documents=True
     )
 
+    return qa
+
+
+def db_qna(db, q):
+    """
+    벡터저장소에서 질문을 검색해서 적절한 답변을 찾아서 답하도록 하는 함수
+    :param db: 벡터스토어
+    :param q: 사용자 질문
+    :return: 추가 질문 유무
+    """
+
+    qa = chat_llm(db)
+
     result = qa(q)
     # print("\n\n{}".format(result))
     # print("\n\n{}".format(result['source_documents'][0].page_content))  # 인덱스 맨 마지막 것이 가장 유사도가 높은거 같음 => 아래 실행해보니 아님. 후보군 3개 중에서 짜집기해서 답 내는 것 같음
 
+    return input("\n\nY: 계속 질문한다.\nN: 프로그램 종료\n입력: ")
 
-def use_openai_embedding(docs, save_directory):
+
+def document_embedding(docs, model, save_directory):
     """
-    openai embedding model을 사용하여 벡터저장소 (vectorStore)에 저장하려고 할 때 사용하는 함수
+    OpenAI Embedding 모델을 사용하여 문서 임베딩하여 Chroma 벡터저장소(VectorStore)에 저장하는 함수
+    :param model: 임베딩 모델 종류
+    :param save_directory: 벡터저장소 저장 경로
     :param docs: 분할된 문서
-    :param save_directory: 저장 경로
     :return: 
     """
 
-    # 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
-    os.environ['OPENAI_API_KEY'] = input('발급 받은 OpenAI API Key를 입력해주세요: ')
+    print("\n잠시만 기다려주세요.\n\n")
 
     # 벡터저장소가 이미 존재하는지 확인
     if os.path.exists(save_directory):
         shutil.rmtree(save_directory)
         print(f"디렉토리 {save_directory}가 삭제되었습니다.\n")
 
+    if isinstance(model, OpenAIEmbeddings):
+        # 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
+        # os.environ['OPENAI_API_KEY'] = input('발급 받은 OpenAI API Key를 입력해주세요: ')
+        os.environ['OPENAI_API_KEY'] = "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"  # 테스트 버전일 때
+
     print("문서 벡터화를 시작합니다. ")
-    Chroma.from_documents(docs, OpenAIEmbeddings(), persist_directory=save_directory)
-
-    print("새로운 Chroma 데이터베이스가 생성되었습니다.")
-
-
-def rc_text_split(corpus):
-    """
-    RecursiveCharacterTextSplitter를 사용하여 문서를 분할하도록 하는 함수
-    :param corpus:
-    :return:
-    """
-    rc_text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=100,
-        length_function=tiktoken_len,  # 단위를 토큰으로 함
-    )
-
-    texts = rc_text_splitter.split_text(loader)
-    # print(texts[0])
-
-    text_documents = rc_text_splitter.create_documents(texts)  # document로 만들기
-
-    # print(f"글자 수 {len(text_documents[0].page_content)}")
-    # print(f"토큰 수 {tiktoken_len(text_documents[0].page_content)}")
-
-    # token 사이즈 출력
-
-    token_list = []
-    for i in range(len(text_documents)):
-        token_list.append(tiktoken_len(text_documents[i].page_content))
-
-    # print(token_list)
-    # print(len(token_list))
+    Chroma.from_documents(docs, model, persist_directory=save_directory)
+    print("새로운 Chroma 데이터베이스가 생성되었습니다.\n")
 
 
 def c_text_split(corpus):
@@ -128,40 +110,60 @@ def c_text_split(corpus):
         length_function=tiktoken_len,  # 단위를 토큰으로 함
     )
 
-    texts = c_text_splitter.split_text(loader)
-    # print(texts[0])
+    texts = c_text_splitter.split_text(corpus)
 
     text_documents = c_text_splitter.create_documents(texts)  # document로 만들기
 
-    # print(f"글자 수 {len(text_documents[0].page_content)}")
-    # print(f"토큰 수 {tiktoken_len(text_documents[0].page_content)}")
-
     # token 사이즈 출력
 
-    token_list = []
-    for i in range(len(text_documents)):
-        token_list.append(tiktoken_len(text_documents[i].page_content))
+    # token_list = []
+    # for i in range(len(text_documents)):
+    #     token_list.append(tiktoken_len(text_documents[i].page_content))
 
     # print(token_list)
-    # print(len(token_list))
 
-    # 문서 임베딩 작업 결과 벡터저장소에 저장
-    # use_openai_embedding(text_documents, save_directory="./chroma_db")
+    return text_documents
+
+
+def run():
+    """
+    챗봇 시작
+    Document Load -> Text Splitter -> Ducument Embedding -> VectorStore save -> QA
+    :return:
+    """
+
+    # 문서 업로드
+    with open("corpus/정시 모집요강(동의대) 전처리 결과.txt", "r", encoding="utf-8") as file:
+        loader = file.read()
+
+        # 문서 분할
+        chunk = c_text_split(loader)
+
+        # 문서 임베딩 및 벡터스토어 저장
+        embedding_model_number = input(
+            "문서 임베딩에 사용할 임베딩 모델을 고르시오. 고르지 않을 경우 HuggingFaceEmbeddings 모델을 기본으로 사용합니다.\n1: OpenAIEmbeddings()\n2: HuggingFaceEmbeddings()\n\n 선택 번호 : ")
+
+        if embedding_model_number == 1:
+            model = OpenAIEmbeddings()
+        else:
+            model_name = "jhgan/ko-sbert-nli"  # 한국어 모델
+            model_kwargs = {'device': 'cpu'}  # cpu를 사용하기 위해 설정
+            encode_kwargs = {'normalize_embeddings': True}
+            model = HuggingFaceEmbeddings(
+                model_name=model_name,
+                model_kwargs=model_kwargs,
+                encode_kwargs=encode_kwargs
+            )
+
+        document_embedding(chunk, model, save_directory="./chroma_db")
+
+        # 정보 검색
+        db = Chroma(persist_directory="./chroma_db", embedding_function=model)
+        check = 'Y'  # 0이면 질문 가능
+        while check == 'Y' or check == 'y':
+            query = input("질문을 입력하세요 : ")
+            check = db_qna(db, query)
 
 
 if __name__ == "__main__":
-    with open("corpus/정시 모집요강(동의대) 전처리 결과.txt", "r", encoding="utf-8") as f:
-        # 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
-        # os.environ['OPENAI_API_KEY'] = input('발급 받은 OpenAI API Key를 입력해주세요: ')  # 정규 버전일 때
-        os.environ['OPENAI_API_KEY'] = "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"  # 테스트 버전일 때
-
-        loader = f.read()
-        
-        # 문서 임베딩 작업할 때 사용할 splitter에 맞추어 주석 풀기
-        # c_text_split(loader)  # CharacterTextSplitter 사용할 때
-        # rc_text_split(loader)  # RecursiveCharacterTextSplitter 사용할 때
-
-        # 이 부분만 수정해서 사용하면 됨
-        query = "원서 접수 날짜는 언제야?"
-        # query = "디자인조형학과의 실기종목과 준비물, 실기주에, 화지크기, 시간에 대해서 알려줘"
-        db_qna(query)
+    run()
